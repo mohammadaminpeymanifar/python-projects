e236f2f169f59aaf3b25b69fda7d127dfd92ac86
Mohammad Amin <98895840+mohammadaminpeymanifar@users.noreply.github.com>
2026-05-17 01:35:01 +0330

    MyLibrary - Python Package Project
    📚 Project Description
    
    This project is a simple Library Management System developed using Python Modules and Packages. The system allows users to:
    
    Add books
    Remove books
    Search books
    Show all books
    
    The project is also prepared as an installable Python package using pip install ..

---
+++ Project/README.md.txt
@@ -0,0 +1,14 @@
+# MyLibrary Package
+
+Simple library management system built with Python packages and modules.
+
+## Features
+- Add books
+- Remove books
+- Search books
+- Show all books
+
+## Installation
+
+```bash
+pip install .
---
+++ Project/mylibrary/Library.py
@@ -0,0 +1,21 @@
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, title, author):
+        self.books.append({"title": title, "author": author})
+
+    def remove_book(self, title):
+        for book in self.books:
+            if book["title"] == title:
+                self.books.remove(book)
+                return
+
+    def search_book(self, title):
+        for book in self.books:
+            if book["title"] == title:
+                return book
+        return None
+
+    def show_books(self):
+        return self.books
